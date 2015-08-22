cca9d04c0235a16cec18e42c73447077e06a59cc
Ewerton <shakamaiden@gmail.com>
2015-08-22 17:30:04 -0300

    Idle Animation Working ( I stopped being Stupid for a second) Collisions are not working. Maybe because how the movement of the player is being used.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -32,24 +32,32 @@ public class PlayerMovement : MonoBehaviour {
             transform.Translate(0, playerSpeed * Time.deltaTime, 0);
             ChangeAnimationState(2);
         }
+        else if (Input.GetKeyUp("up"))
+            ChangeAnimationState(5);
 
         if (Input.GetKey("down"))//Press up arrow key to move forward on the Y AXIS
         {
             transform.Translate(0, -playerSpeed * Time.deltaTime, 0);
             ChangeAnimationState(1);
         }
+        else if (Input.GetKeyUp("down"))
+            ChangeAnimationState(0);
 
         if (Input.GetKey("left"))//Press up arrow key to move forward on the Y AXIS
         {
             transform.Translate(-playerSpeed * Time.deltaTime, 0, 0);
             ChangeAnimationState(4);
         }
+        else if (Input.GetKeyUp("left"))
+            ChangeAnimationState(7);
 
         if (Input.GetKey("right"))//Press up arrow key to move forward on the Y AXIS
         {
             transform.Translate(playerSpeed * Time.deltaTime, 0, 0);
             ChangeAnimationState(3);
         }
+        else if (Input.GetKeyUp("right"))
+            ChangeAnimationState(6);
 	}
 
     void OnCollisionEnter2D(Collision2D coll)
