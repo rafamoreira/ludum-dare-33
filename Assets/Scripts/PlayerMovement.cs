064cd9bf4bf6e320d9ca0b3b8ae9d42ac01f2185
Ewerton <shakamaiden@gmail.com>
2015-08-22 17:17:41 -0300

    Player Animations - Can't work Idle(i'm to stupid)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,9 +8,17 @@ public class PlayerMovement : MonoBehaviour {
     bool collidingB;
     bool collidingR;
     bool collidingT;
+    private Animator animator;
+
 
 	// Use this for initialization
-	void Start () {
+    void Awake()
+    {
+        animator = GetComponent<Animator>();
+        ChangeAnimationState(0);
+    }
+    
+    void Start () {
         collidingL = false;
         collidingB = false;
         collidingR = false;
@@ -22,23 +30,44 @@ public class PlayerMovement : MonoBehaviour {
         if (Input.GetKey("up"))//Press up arrow key to move forward on the Y AXIS
         {
             transform.Translate(0, playerSpeed * Time.deltaTime, 0);
+            ChangeAnimationState(2);
         }
+
         if (Input.GetKey("down"))//Press up arrow key to move forward on the Y AXIS
         {
             transform.Translate(0, -playerSpeed * Time.deltaTime, 0);
+            ChangeAnimationState(1);
         }
+
         if (Input.GetKey("left"))//Press up arrow key to move forward on the Y AXIS
         {
             transform.Translate(-playerSpeed * Time.deltaTime, 0, 0);
+            ChangeAnimationState(4);
         }
+
         if (Input.GetKey("right"))//Press up arrow key to move forward on the Y AXIS
         {
             transform.Translate(playerSpeed * Time.deltaTime, 0, 0);
+            ChangeAnimationState(3);
         }
 	}
 
     void OnCollisionEnter2D(Collision2D coll)
     {
 
+    }
+
+    void ChangeAnimationState(int valeu)
+    {
+        animator.SetInteger("AnimState", valeu);
+        /* 0 - Idle
+         * 1 - Front
+         * 2 - Back
+         * 3 - Right
+         * 4 - Left
+         * 5 - Idle Back
+         * 6 - Idle Right
+         * 7 - Idle left 
+         */
     }
 }
